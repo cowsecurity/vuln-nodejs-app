82ae1a38fc56054267382f8e589f6b6d15b64aa1
HoLee Cow <74612612+cowsecurity@users.noreply.github.com>
2026-06-23 21:42:53 +0530

    Update auth_controller.js

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -90,7 +90,7 @@ const login_post = (req, res) => {
               res.status(200).send('/');
             }
           } else {
-            res.status(403).send('Invalid username/password.');
+            res.status(403).send('Invalid username/pasword.');
           }
         });
   } else {
